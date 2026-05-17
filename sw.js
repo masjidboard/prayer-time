31200b3aae18a811a10ff437008dcbbb89448d96
masjidboard <masjidboard820@gmail.com>
2026-05-17 10:19:46 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+// Prayer Time - Service Worker
+const CACHE_NAME = 'prayer-time-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/masjidboard.html',
+  '/icon-512.png',
+  '/icon-192.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
